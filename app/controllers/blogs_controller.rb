4ed35e24c4223b1f4d3457f9b594fb0e8eb981cc
reckyy <srreki98@gmail.com>
2023-12-19 22:39:15 +0900

    🐛 fix: 他人の秘密のブログにアクセスできないように修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,7 +9,9 @@ class BlogsController < ApplicationController
     @blogs = Blog.search(params[:term]).published.default_order
   end
 
-  def show; end
+  def show
+    raise ActiveRecord::RecordNotFound if @blog.secret? && !@blog.owned_by?(current_user)
+  end
 
   def new
     @blog = Blog.new
